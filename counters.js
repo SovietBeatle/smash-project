a10183b6b04adf1500c6fb47e256b54bd0cd96b3
SovietBeatle <kolby.ellis123@gmail.com>
2023-03-29 19:45:04 -0700

    refactor entire counter mechanism

--- counters.js
+++ counters.js
@@ -1,154 +1,176 @@
-//Mario//
+var rects = [
+  kingKRoolRect,
+  bowserRect,
+  pokemonTrainerRect,
+  ridleyRect,
+  kingDededeRect,
+  lucinaRect,
+  marthRect,
+  shulkRect,
+  cloudRect,
+  palutenaRect,
+  marioRect
+];
 
-marioRect.addEventListener("click", function() {
-  if (peachRect.getAttribute("fill") === "#0F000" && kingKRoolRect.getAttribute("fill") === "#0F000" && bowserRect.getAttribute("fill") === "#0F000" && pokemonTrainerRect.getAttribute("fill") === "#0F000" && ridleyRect.getAttribute("fill") === "#0F000" && kingDededeRect.getAttribute("fill") === "#0F000"&& lucinaRect.getAttribute("fill") === "#0F000" && marthRect.getAttribute("fill") === "#0F000" && shulkRect.getAttribute("fill") === "#0F000" && cloudRect.getAttribute("fill") === "#0F000" && palutenaRect.getAttribute("fill") === "#0F000") {
-  peachRect.setAttribute("stroke", "black");
-  peachRect.setAttribute("fill", "#fff");
-  peachRect.setAttribute("opacity", "0");
-  kingKRoolRect.setAttribute("stroke", "black");
-  kingKRoolRect.setAttribute("fill", "#fff");
-  kingKRoolRect.setAttribute("opacity", "0");
-  bowserRect.setAttribute("stroke", "black");
-  bowserRect.setAttribute("fill", "#fff");
-  bowserRect.setAttribute("opacity", "0");
-  pokemonTrainerRect.setAttribute("stroke", "black");
-  pokemonTrainerRect.setAttribute("fill", "#fff");
-  pokemonTrainerRect.setAttribute("opacity", "0");
-  ridleyRect.setAttribute("stroke", "black");
-  ridleyRect.setAttribute("fill", "#fff");
-  ridleyRect.setAttribute("opacity", "0");
-  kingDededeRect.setAttribute("stroke", "black");
-  kingDededeRect.setAttribute("fill", "#fff");
-  kingDededeRect.setAttribute("opacity", "0");
-  lucinaRect.setAttribute("stroke", "black");
-  lucinaRect.setAttribute("fill", "#fff");
-  lucinaRect.setAttribute("opacity", "0");
-  marthRect.setAttribute("stroke", "black");
-  marthRect.setAttribute("fill", "#fff");
-  marthRect.setAttribute("opacity", "0");
-  shulkRect.setAttribute("stroke", "black");
-  shulkRect.setAttribute("fill", "#fff");
-  shulkRect.setAttribute("opacity", "0");
-  cloudRect.setAttribute("stroke", "black");
-  cloudRect.setAttribute("fill", "#fff");
-  cloudRect.setAttribute("opacity", "0");
-  palutenaRect.setAttribute("stroke", "black");
-  palutenaRect.setAttribute("fill", "#fff");
-  palutenaRect.setAttribute("opacity", "0");
-  marioRect.setAttribute("opacity", "0");
-  } else {
-  peachRect.setAttribute("stroke", "none");
-  peachRect.setAttribute("fill", "#00FF00");
-  peachRect.setAttribute("opacity", "0.5");
-  kingKRoolRect.setAttribute("stroke", "none");
-  kingKRoolRect.setAttribute("fill", "#00FF00");
-  kingKRoolRect.setAttribute("opacity", "0.5");
-  bowserRect.setAttribute("stroke", "none");
-  bowserRect.setAttribute("fill", "#00FF00");
-  bowserRect.setAttribute("opacity", "0.5");
-  pokemonTrainerRect.setAttribute("stroke", "none");
-  pokemonTrainerRect.setAttribute("fill", "#00FF00");
-  pokemonTrainerRect.setAttribute("opacity", "0.5");
-  ridleyRect.setAttribute("stroke", "none");
-  ridleyRect.setAttribute("fill", "#00FF00");
-  ridleyRect.setAttribute("opacity", "0.5");
-  kingDededeRect.setAttribute("stroke", "none");
-  kingDededeRect.setAttribute("fill", "#00FF00");
-  kingDededeRect.setAttribute("opacity", "0.5");
-  lucinaRect.setAttribute("opacity", "0.5");
-  lucinaRect.setAttribute("stroke", "none");
-  lucinaRect.setAttribute("fill", "#FF0000");
-  marthRect.setAttribute("opacity", "0.5");
-  marthRect.setAttribute("stroke", "none");
-  marthRect.setAttribute("fill", "#FF0000");
-  cloudRect.setAttribute("opacity", "0.5");
-  cloudRect.setAttribute("stroke", "none");
-  cloudRect.setAttribute("fill", "#FF0000");
-  shulkRect.setAttribute("opacity", "0.5");
-  shulkRect.setAttribute("stroke", "none");
-  shulkRect.setAttribute("fill", "#FF0000");
-  palutenaRect.setAttribute("opacity", "0.5");
-  palutenaRect.setAttribute("stroke", "none");
-  palutenaRect.setAttribute("fill", "#FF0000");
-  marioRect.setAttribute("opacity", "0.5");
-  }
-  });
+var originalAttributes = {};
+rects.forEach(function(rect) {
+  originalAttributes[rect.id] = {
+    fill: rect.getAttribute("fill"),
+    stroke: rect.getAttribute("stroke"),
+    opacity: rect === marioRect ? "1" : rect.getAttribute("opacity")
+  };
+});
 
-  //Bowser//
-
-  bowserRect.addEventListener("click", function() {
-    if (jigglypuffRect.getAttribute("fill") === "#0F000" && kirbyRect.getAttribute("fill") === "#0F000" && pokemonTrainerRect.getAttribute("fill") === "#0F000" && pokemonTrainerRect.getAttribute("fill") === "#0F000" && littleMacRect.getAttribute("fill") === "#0F000" && mrGameAndWatchRect.getAttribute("fill") === "#0F000"&& youngLinkRect.getAttribute("fill") === "#0F000" && darkSamusRect.getAttribute("fill") === "#0F000" && pikachuRect.getAttribute("fill") === "#0F000" && samusRect.getAttribute("fill") === "#0F000" && nessRect.getAttribute("fill") === "#0F000") {
-    jigglypuffRect.setAttribute("stroke", "black");
-    jigglypuffRect.setAttribute("fill", "#fff");
-    jigglypuffRect.setAttribute("opacity", "0");
-    kirbyRect.setAttribute("stroke", "black");
-    kirbyRect.setAttribute("fill", "#fff");
-    kirbyRect.setAttribute("opacity", "0");
-    pokemonTrainerRect.setAttribute("stroke", "black");
-    pokemonTrainerRect.setAttribute("fill", "#fff");
-    pokemonTrainerRect.setAttribute("opacity", "0");
-    pokemonTrainerRect.setAttribute("stroke", "black");
-    pokemonTrainerRect.setAttribute("fill", "#fff");
-    pokemonTrainerRect.setAttribute("opacity", "0");
-    littleMacRect.setAttribute("stroke", "black");
-    littleMacRect.setAttribute("fill", "#fff");
-    littleMacRect.setAttribute("opacity", "0");
-    mrGameAndWatchRect.setAttribute("stroke", "black");
-    mrGameAndWatchRect.setAttribute("fill", "#fff");
-    mrGameAndWatchRect.setAttribute("opacity", "0");
-    youngLinkRect.setAttribute("stroke", "black");
-    youngLinkRect.setAttribute("fill", "#fff");
-    youngLinkRect.setAttribute("opacity", "0");
-    pokemonTrainerRect.setAttribute("stroke", "black");
-    darkSamusRect.setAttribute("fill", "#fff");
-    darkSamusRect.setAttribute("opacity", "0");
-    pikachuRect.setAttribute("stroke", "black");
-    pikachuRect.setAttribute("fill", "#fff");
-    pikachuRect.setAttribute("opacity", "0");
-    samusRect.setAttribute("stroke", "black");
-    samusRect.setAttribute("fill", "#fff");
-    samusRect.setAttribute("opacity", "0");
-    nessRect.setAttribute("stroke", "black");
-    nessRect.setAttribute("fill", "#fff");
-    nessRect.setAttribute("opacity", "0");
-    bowserRect.setAttribute("opacity", "0");
-    } else {
-    jigglypuffRect.setAttribute("stroke", "none");
-    jigglypuffRect.setAttribute("fill", "#00FF00");
-    jigglypuffRect.setAttribute("opacity", "0.5");
-    kirbyRect.setAttribute("stroke", "none");
-    kirbyRect.setAttribute("fill", "#00FF00");
-    kirbyRect.setAttribute("opacity", "0.5");
-    pokemonTrainerRect.setAttribute("stroke", "none");
-    pokemonTrainerRect.setAttribute("fill", "#00FF00");
-    pokemonTrainerRect.setAttribute("opacity", "0.5");
-    pokemonTrainerRect.setAttribute("stroke", "none");
-    pokemonTrainerRect.setAttribute("fill", "#00FF00");
-    pokemonTrainerRect.setAttribute("opacity", "0.5");
-    littleMacRect.setAttribute("stroke", "none");
-    littleMacRect.setAttribute("fill", "#00FF00");
-    littleMacRect.setAttribute("opacity", "0.5");
-    mrGameAndWatchRect.setAttribute("stroke", "none");
-    mrGameAndWatchRect.setAttribute("fill", "#00FF00");
-    mrGameAndWatchRect.setAttribute("opacity", "0.5");
-    youngLinkRect.setAttribute("opacity", "0.5");
-    youngLinkRect.setAttribute("stroke", "none");
-    youngLinkRect.setAttribute("fill", "#FF0000");
-    darkSamusRect.setAttribute("opacity", "0.5");
-    darkSamusRect.setAttribute("stroke", "none");
-    darkSamusRect.setAttribute("fill", "#FF0000");
-    samusRect.setAttribute("opacity", "0.5");
-    samusRect.setAttribute("stroke", "none");
-    samusRect.setAttribute("fill", "#FF0000");
-    pikachuRect.setAttribute("opacity", "0.5");
-    pikachuRect.setAttribute("stroke", "none");
-    pikachuRect.setAttribute("fill", "#FF0000");
-    nessRect.setAttribute("opacity", "0.5");
-    nessRect.setAttribute("stroke", "none");
-    nessRect.setAttribute("fill", "#FF0000");
-    bowserRect.setAttribute("opacity", "0.5");
-    }
+function toggleMario() {
+  if (marioRect.getAttribute("opacity") === "0.5") {
+    marioRect.setAttribute("opacity", "1");
+    rects.forEach(function(rect) {
+      rect.setAttribute("stroke", originalAttributes[rect.id].stroke);
+      rect.setAttribute("fill", originalAttributes[rect.id].fill);
+      rect.setAttribute("opacity", originalAttributes[rect.id].opacity);
     });
-   
-   
-   
+  } else {
+    marioRect.setAttribute("opacity", "0.5");
+    rects.forEach(function(rect) {
+      if (rect !== marioRect) {
+        rect.setAttribute("stroke", "none");
+        if (rect === lucinaRect ||
+            rect === marthRect ||
+            rect === shulkRect ||
+            rect === cloudRect ||
+            rect === palutenaRect) {
+          rect.setAttribute("fill", "#FF0000");
+        } else {
+          rect.setAttribute("fill", "#00FF00");
+        }
+        rect.setAttribute("opacity", "0.5");
+      }
+    });
+  }
+}
+
+marioRect.addEventListener("click", toggleMario);
+
+
+var originalAttributes = {};
+rects.forEach(function(rect) {
+  originalAttributes[rect.id] = {
+    fill: rect.getAttribute("fill"),
+    stroke: rect.getAttribute("stroke"),
+    opacity: rect.getAttribute("opacity")
+  };
+});
+
+function toggleBowser() {
+  if (bowserRect.getAttribute("opacity") === "0.5") {
+    bowserRect.setAttribute("opacity", "1");
+    rects.forEach(function(rect) {
+      rect.setAttribute("stroke", originalAttributes[rect.id].stroke);
+      rect.setAttribute("fill", originalAttributes[rect.id].fill);
+      rect.setAttribute("opacity", originalAttributes[rect.id].opacity);
+    });
+  } else {
+    bowserRect.setAttribute("opacity", "0.5");
+    rects.forEach(function(rect) {
+      if (rect !== bowserRect) {
+        rect.setAttribute("stroke", "none");
+        if (rect === lucinaRect ||
+            rect === marthRect ||
+            rect === shulkRect ||
+            rect === cloudRect ||
+            rect === palutenaRect) {
+          rect.setAttribute("fill", "#FF0000");
+        } else {
+          rect.setAttribute("fill", "#00FF00");
+        }
+        rect.setAttribute("opacity", "0.5");
+      }
+    });
+  }
+}
+
+bowserRect.addEventListener("click", toggleBowser);
+
+var rects = [
+  kingKRoolRect,
+  ridleyRect,
+  pokemonTrainerRect,
+  bowserRect,
+  kingDededeRect,
+  lucinaRect,
+  marthRect,
+  shulkRect,
+  cloudRect,
+  palutenaRect,
+  marioRect
+];
+
+var originalAttributes = {};
+rects.forEach(function(rect) {
+  originalAttributes[rect.id] = {
+    fill: rect.getAttribute("fill"),
+    stroke: rect.getAttribute("stroke"),
+    opacity: rect.getAttribute("opacity")
+  };
+});
+
+function toggleKingKrool() {
+  if (kingKRoolRect.getAttribute("opacity") === "0.5") {
+    kingKRoolRect.setAttribute("opacity", "1");
+    rects.forEach(function(rect) {
+      rect.setAttribute("stroke", originalAttributes[rect.id].stroke);
+      rect.setAttribute("fill", originalAttributes[rect.id].fill);
+      rect.setAttribute("opacity", originalAttributes[rect.id].opacity);
+    });
+  } else {
+    kingKRoolRect.setAttribute("opacity", "0.5");
+    rects.forEach(function(rect) {
+      if (rect !== kingKRoolRect) {
+        rect.setAttribute("stroke", "none");
+        if (rect === lucinaRect ||
+            rect === marthRect ||
+            rect === shulkRect ||
+            rect === cloudRect ||
+            rect === palutenaRect) {
+          rect.setAttribute("fill", "#FF0000");
+        } else {
+          rect.setAttribute("fill", "#00FF00");
+        }
+        rect.setAttribute("opacity", "0.5");
+      }
+    });
+  }
+}
+
+kingKRoolRect.addEventListener("click", toggleKingKrool);
+
+
+function toggleRidley() {
+  if (ridleyRect.getAttribute("opacity") === "0.5") {
+    ridleyRect.setAttribute("opacity", "1");
+    rects.forEach(function(rect) {
+      rect.setAttribute("stroke", originalAttributes[rect.id].stroke);
+      rect.setAttribute("fill", originalAttributes[rect.id].fill);
+      rect.setAttribute("opacity", originalAttributes[rect.id].opacity);
+    });
+  } else {
+    ridleyRect.setAttribute("opacity", "0.5");
+    rects.forEach(function(rect) {
+      if (rect !== ridleyRect) {
+        rect.setAttribute("stroke", "none");
+        if (rect === lucinaRect ||
+            rect === marthRect ||
+            rect === shulkRect ||
+            rect === cloudRect ||
+            rect === palutenaRect) {
+          rect.setAttribute("fill", "#FF0000");
+        } else {
+          rect.setAttribute("fill", "#00FF00");
+        }
+        rect.setAttribute("opacity", "0.5");
+      }
+    });
+  }
+}
+
+ridleyRect.addEventListener("click", toggleRidley);
